6f4ce1e446d16837705f9792239352c9e70d4509
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2024-04-21 10:42:38 +0000

    files updated

--- push_swap/srcs/push_swap/ft_error_print.c
+++ push_swap/srcs/push_swap/ft_error_print.c
@@ -4,6 +4,6 @@
 void	ft_error(void)
 {	
 	ft_putstr_fd("Error!\n", 2);
-	ft_putstr_fd("check if integer is double or is not integer type <0 to 9>! or invalid command.\n", 2);
+	ft_putstr_fd("check if integer is double or is not integer type <0 to 9>! or invalid command or empty input.\n", 2);
 	exit(1);
 }
