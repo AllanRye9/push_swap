b7bc50e26063a1cf313788c5de369f47981c4c46
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2024-06-10 13:19:38 +0000

    file updated

--- push_swap/srcs/checker/checker_utils.c
+++
@@ -1,7 +0,0 @@
-#include "../../includes/push_swap.h"
-
-void	ft_error_ch(void)
-{
-	write(1, "Error!\n", 8);
-	exit(1);
-}
--- push_swap/srcs/push_swap/ft_error_print.c
+++
@@ -1,9 +0,0 @@
-#include "../../includes/push_swap.h"
-
-//Here an error message is being printed out
-void	ft_error(void)
-{	
-	ft_putstr_fd("Error!\n", 2);
-	ft_putstr_fd("check if integer is double or is not integer type <0 to 9>! or invalid command or empty input.\n", 2);
-	exit(1);
-}
